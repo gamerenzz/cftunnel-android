edbb51797cf630e0648ad177e654f3f9e262ee78
gamerenzz <36974615+gamerenzz@users.noreply.github.com>
2026-05-30 09:10:15 +0800

    Create MainActivity.kt

---
+++ app/src/main/java/com/qingchen/cftunnel/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.qingchen.cftunnel
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MaterialTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("cftunnel Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Welcome to $name!\nCI/CD Environment Setup Successfully.",
+            style = MaterialTheme.typography.titleMedium,
+            modifier = modifier
+        )
+    }
+}
